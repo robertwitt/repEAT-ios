124bc8061034bec4f7ef563387c988f20932adb3
Robert Witt <46402879+robertwitt@users.noreply.github.com>
2020-10-03 12:49:15 +0200

    chore: Project template (#7)
    * Setup project template
    
    * Update .gitignore
    
    * Setup SwiftLint

---
+++ repEAT/Scenes/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  repEAT
+//
+//  Created by Witt, Robert on 03.10.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
