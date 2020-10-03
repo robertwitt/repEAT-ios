c05f8f4d18b868417df83eb244898085577d061a
Robert Witt <robert_witt@icloud.com>
2020-10-03 12:46:31 +0200

    Setup SwiftLint

--- repEATTests/repEATTests.swift
+++ repEATTests/repEATTests.swift
@@ -8,7 +8,9 @@
 import XCTest
 @testable import repEAT
 
+// swiftlint:disable type_name
 class repEATTests: XCTestCase {
+// swiftlint:enable type_name
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
